09125fe9b7d02d65d8b0ae364e33eb14b2a0d905
Aiman Haziq <136099677+AimanShazman@users.noreply.github.com>
2024-09-21 18:18:44 +0900

    add Player Movement
    -added MovePlayer() to control the player movement
    -added left and right boundary

--- Balloon Game/Assets/Scripts/PlayerController.cs
+++ Balloon Game/Assets/Scripts/PlayerController.cs
@@ -1,37 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float backgroundWidth;
-    // private Vector3 startPos;
+    private float thirdOfGroundWidth;
     private Vector3 playerMovement;
-
-    // private Vector3 playerMovement;
+    private Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        // startPos = transform.position;
-        backgroundWidth = GameObject.Find("Background").GetComponent<BoxCollider>().size.x / 3;
-        playerMovement = new Vector3(backgroundWidth, 0, 0);
-
+        thirdOfGroundWidth = GameObject.Find("Ground").transform.localScale.x / 3;
+        playerMovement = new Vector3(thirdOfGroundWidth, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         if(Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = transform.position - playerMovement;
+            //left Boundary
+            if(transform.position.x == -thirdOfGroundWidth)
+            {
+                transform.position = new Vector3(-thirdOfGroundWidth, transform.position.y, transform.position.z);
+            } else
+            {
+                transform.position = transform.position - playerMovement;
+            }
         }
         
         if(Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = transform.position + playerMovement;
+            //right Boundary
+            if(transform.position.x == thirdOfGroundWidth)
+            {
+                transform.position = new Vector3(thirdOfGroundWidth, transform.position.y, transform.position.z);
+            } else
+            {
+                transform.position = transform.position + playerMovement;
+            }
         }
-
-        
     }
 }
